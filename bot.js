246fc07d9c0346064b9f36ca320241e7f88a6825
Milk-Cool <milan-pl@mail.ru>
2025-01-15 20:44:27 +0300

    some commands

--- bot.js
+++ bot.js
@@ -9,13 +9,22 @@ import * as lk from "./lk.js";
 
 const { TOKEN, HOST } = process.env;
 
+const buttonLogin = { text: Strings.LOG_IN_BUTTON, web_app: { url: HOST } };
 const buttonsLogin = {
     keyboard: [
-        [
-            { text: Strings.LOG_IN_BUTTON, web_app: { url: HOST } }
-        ]
+        [buttonLogin]
     ]
 };
+
+const buttonUpdate = { text: Strings.UPDATE_BALANCE, web_app: { url: HOST } };
+const buttonAllEvents = { text: Strings.ALL_EVENTS };
+const buttonBalance = { text: Strings.BALANCE };
+const buttonsMain = {
+    keyboard: [
+        [buttonUpdate],
+        [buttonAllEvents, buttonBalance]
+    ]
+}
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 const app = express();
@@ -33,14 +42,39 @@ const prepare = async msg => {
     return true;
 }
 
+const chooseAction = async id =>
+    await bot.sendMessage(id, Strings.CHOOSE_ACTION, {
+        reply_markup: buttonsMain
+    });
+
+/**
+ * @param {number} id Telegram ID
+ * @param {Bets.Event[]} events Events
+ */
+const sendEvents = async (id, events) => {
+    if(events.length == 0)
+        return await bot.sendMessage(id, Strings.NO_EVENTS);
+    for(const event of events) {
+        const date = new Date(event.until);
+        await bot.sendMessage(id, Strings.NEW_EVENT
+            .replace("[[description]]", event.description)
+            .replace("[[outcome_left]]", event.outcome_left)
+            .replace("[[outcome_left_chance]]", event.outcome_left_chance)
+            .replace("[[outcome_right]]", event.outcome_right)
+            .replace("[[outcome_right_chance]]", event.outcome_right_chance)
+            .replace("[[until]]", `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`)
+        );
+    }
+};
+
 bot.onText(/^\/start$/, async msg => {
     if(!await prepare(msg)) return;
     if(states[msg.chat.id] === State.TO_LOGIN)
-        bot.sendMessage(msg.chat.id, Strings.HELLO, {
+        await bot.sendMessage(msg.chat.id, Strings.HELLO, {
             reply_markup: buttonsLogin
         });
     else if(states[msg.chat.id] === State.LOGGED_IN)
-        null; // TODO: send a message explaining what the menu does
+        await chooseAction(msg.chat.id);
 });
 bot.on("message", async msg => {
     if(!await prepare(msg)) return;
@@ -48,7 +82,7 @@ bot.on("message", async msg => {
     const [username, password] = msg.web_app_data.data.split(":");
     const previous = await Bets.getUserByTelegramID(msg.chat.id);
     if(await Bets.getUserByUsername(username) && !previous) {
-        bot.sendMessage(msg.chat.id, Strings.ALREADY_LOGGED_IN);
+        await bot.sendMessage(msg.chat.id, Strings.ALREADY_LOGGED_IN);
         return;
     }
     await bot.sendMessage(msg.chat.id, Strings.LOGGING_IN);
@@ -59,13 +93,32 @@ bot.on("message", async msg => {
         if(!previous) await Bets.createUser(username, msg.chat.id);
         await Bets.updateUserBalance(username, points);
         states[msg.chat.id] = State.LOGGED_IN;
-        bot.sendMessage(msg.chat.id, previous ? Strings.UPDATED_BALANCE : Strings.LOGGED_IN);
-        // sendEvents(msg.chat.id, await Bets.getAllOngoingEvents());
+        await bot.sendMessage(msg.chat.id, previous ? Strings.UPDATED_BALANCE : Strings.LOGGED_IN);
+        await sendEvents(msg.chat.id, await Bets.getAllOngoingEvents());
+        await chooseAction(msg.chat.id);
     } catch(_) {
-        bot.sendMessage(msg.chat.id, Strings.WRONG_CREDENTIALS);
+        await bot.sendMessage(msg.chat.id, Strings.WRONG_CREDENTIALS);
         states[msg.chat.id] = State.TO_LOGIN;
     }
-})
+});
+
+bot.on("message", async msg => {
+    if(!await prepare(msg)) return;
+    if(states[msg.chat.id] !== State.LOGGED_IN) return;
+    switch(msg.text) {
+        case Strings.BALANCE:
+            const user = await Bets.getUserByTelegramID(msg.chat.id);
+            await bot.sendMessage(msg.chat.id, Strings.BALANCE_IS + user.balance.toString());
+            break;
+        case Strings.ALL_EVENTS:
+            await sendEvents(msg.chat.id, await Bets.getAllOngoingEvents());
+            break;
+        case "/start":
+            break;
+        default:
+            await chooseAction(msg.chat.id);
+    }
+});
 
 app.get("/", (_req, res) => res.sendFile(join(import.meta.dirname, "login.html")));
 
--- strings.js
+++ strings.js
@@ -9,4 +9,14 @@ export class Strings {
     static WRONG_CREDENTIALS = "Неверные данные :(";
     static ALREADY_LOGGED_IN = `Под твоим аккаунтом уже вошли! Если считаешь, что это ошибка, напиши ${ADMIN}`;
     static UPDATED_BALANCE = "Обновили баланс!";
+
+    static ALL_EVENTS = "Все события";
+    static BALANCE = "Проверить баланс";
+    static UPDATE_BALANCE = "Обновить баланс";
+    static CHOOSE_ACTION = "Выберите действие:";
+
+    static BALANCE_IS = "Твой баланс:\n";
+
+    static NEW_EVENT = "Новое событие!\n\n[[description]]\n[[outcome_left]] - [[outcome_left_chance]]%\n[[outcome_right]] - [[outcome_right_chance]]%\nДо [[until]]";
+    static NO_EVENTS = "Сейчас событий нет :(";
 }
